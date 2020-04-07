4199ea1f8d37b1b191caeac4d55abed25c5cdbfa
ThisLightMan <birdkage56@gmail.com>
2020-04-06 22:44:12 -0500

    Added more pages

--- routes/our-work.js
+++ routes/our-work.js
@@ -5,7 +5,20 @@ router.get('/discord-bots', (req, res) => {
     res.render('discord_bots');
 })
 
+router.get('/spigot-plugins', (req, res) => {
+    res.render('spigot_plugins');
+})
+
 router.get('/graphics', (req, res) => {
     res.render('graphics');
 })
+
+router.get('/minecraft-builds', (req, res) => {
+    res.render('minecraft_builds');
+})
+
+router.get('/corebot-addons', (req, res) => {
+    res.render('corebot_addons');
+})
+
 module.exports = router;
