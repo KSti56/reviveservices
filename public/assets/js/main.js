ba414896b11e924a05be3ccf20f104dd703f64c2
ThisLightMan <birdkage56@gmail.com>
2020-04-09 14:00:33 -0500

    Added mobile navigation

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,22 @@
-const toggleNavigation = () => {
-    $("nav").toggleClass("responsive");
+const mobileNavigation = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector("nav > .links");
+
+    const links = document.querySelectorAll("nav > .links > a, nav > .links button");
+
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        links.forEach((link, i) => {
+            if (link.style.animation)
+                link.style.animation = ''
+            else link.style.animation = 'linkFade 3s ease forwards ' + (i / 3) + 's';
+        })
+
+        burger.classList.toggle("x-toggled");
+    })
+}
+
+const app = {
+    mobileNavgation: mobileNavigation()
 }
